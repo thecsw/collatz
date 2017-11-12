5c1a4407eb00c4a8c511707740445123beb28482
jarhill0 <jarhill0@gmail.com>
2017-11-12 12:12:55 -0800

    Rework authorization to use a separate file; other changes:
    - instead of repeatedly calling int(command), assign the result to a variable and use that
    - Validate number as being positive
    - Only say "Not a number!" when the input is not a number
    - Don't encode already-decoded utf-8 text
    - '/HELP' counts as '/help'
    - Open png file in read-binary mode
    - Only catch Exception, not BaseException

--- collatz.py
+++ collatz.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import telepot
 from telepot.loop import MessageLoop
 
-key = 'YOUR TELEGRAM API KEY'
-bot = telepot.Bot(key)
+import config
+
+bot = telepot.Bot(config.token)
 sys.setrecursionlimit(10000)
 steps = big = bigin = 0
 sut = []
@@ -33,36 +34,46 @@ def coll(n):
 def handle(msg):
     global steps, seq, sut, big, bigin
     user_id = msg['chat']['id']
-    command = msg['text'].encode('utf-8')
-    if command == '/help':
+    command = msg['text']
+    if command.lower() == '/help':
         bot.sendMessage(user_id, 'Write any natural number and bot will send you the number of steps required to reach 1\n\
 Also here are some interesting values to try out:\n\
         27, 97, 871, 6171, 77031, 837799, 8400511, 670617279')
         return
     try:
-        coll(int(command))
+        num = int(command)
+
+    except ValueError:
+        bot.sendMessage(user_id, 'Not a number!')
+        return
+    try:
+        if num < 0:
+            bot.sendMessage(user_id, 'Number but be positive.')
+            return
+        coll(num)
         for i in range(0, steps + 1):
             seq.append(i)
         plt.plot(seq, sut, linestyle='-', linewidth=1)
         plt.xlabel('Number of steps')
         plt.ylabel('Value of a step')
-        plt.title('{} steps for {} to reach 1'.format(steps, int(command)))
-        plt.savefig('Graph{}.png'.format(int(command)), dpi=300, format='png')
-        plt.savefig('Graph{}.svg'.format(int(command)), dpi=300, format='svg')
+        plt.title('{} steps for {} to reach 1'.format(steps, num))
+        plt.savefig('Graph{}.png'.format(num), dpi=300, format='png')
+        plt.savefig('Graph{}.svg'.format(num), dpi=300, format='svg')
         bot.sendMessage(user_id, 'The number of steps: {}'.format(steps))
         bot.sendMessage(user_id, 'Biggest value {} during step {}'.format(big, bigin))
         bot.sendMessage(user_id, 'Uploading the graph...')
-        bot.sendPhoto(user_id, open('Graph{}.png'.format(int(command))))
-        bot.sendDocument(user_id, open('Graph{}.svg'.format(int(command))))
+        bot.sendPhoto(user_id, open('Graph{}.png'.format(num), 'rb'))
+        bot.sendDocument(user_id, open('Graph{}.svg'.format(num)))
         bot.sendMessage(user_id, 'Thank you for using collatzbot!')
-        os.remove('Graph{}.png'.format(int(command)))
-        os.remove('Graph{}.svg'.format(int(command)))
+        os.remove('Graph{}.png'.format(num))
+        os.remove('Graph{}.svg'.format(num))
         steps = big = bigin = 0
         sut = []
         seq = []
         plt.clf()
-    except:
-        bot.sendMessage(user_id, 'Not a number!')
+
+    except Exception as e:
+        bot.sendMessage(user_id, 'Error.')
 
 
 MessageLoop(bot, handle).run_as_thread()
