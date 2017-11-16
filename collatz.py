1aa23cfaca2f0bd3f052625e578a5e963327f070
thecsw <sagindyk.urazayev@protonmail.ch>
2017-11-16 19:04:54 +0400

    Deleted the option to build graph of steps and added annotations to the normal graphs

--- collatz.py
+++ collatz.py
@@ -8,7 +8,6 @@ import config
 
 bot = telepot.Bot(config.token)
 
-
 def coll(n):
     steps = [n]
     while n != 1:
@@ -20,58 +19,28 @@ def coll(n):
     steps.pop()
     return steps
 
-def iterate(n):
-    if n > 50000:
-        return
-    fullSteps=[]
-    for i in range(1, n+1):
-        fullSteps.append(len(coll(i)))
-    make_graph(n, fullSteps, 'scatter')
-
-def make_graph(num, steps, style):
-    if style=='plot':
-        plt.plot(range(len(steps)), steps, linestyle='-', linewidth=1)
-    if style=='scatter':
-        plt.scatter(range(len(steps)), steps, linestyle='-', s=1)
+def make_graph(num, steps, bigIn, big):
+    plt.plot(range(len(steps)), steps, linestyle='-', linewidth=1)
     plt.xlabel('Number of steps')
     plt.ylabel('Value of a step')
     plt.title('{} steps for {} to reach 1'.format(len(steps), num))
+    plt.annotate('Highest step',
+                 xy=(bigIn, big),
+                 xytext=(bigIn+(len(steps)/20), big-(big/20)),
+                 arrowprops=dict(arrowstyle='->'),
+    )
     plt.savefig('Graph{}.png'.format(num), dpi=300, format='png')
     plt.savefig('Graph{}.svg'.format(num), dpi=300, format='svg')
     plt.clf()
 
-
 def handle(msg):
     user_id = msg['chat']['id']
-    command = msg['text'].encode('utf-8')
-    if command.lower() == '/help':
+    command = msg['text'].encode('utf-8').lower()
+
+    if command == '/help':
         bot.sendMessage(user_id, 'Write any natural number and bot will send you the number of steps required to reach 1\n\
 Also here are some interesting values to try out:\n\
         27, 97, 871, 6171, 77031, 837799, 8400511, 670617279')
-        return
-
-    if command[0]=='/' and command[1]=='g' and command[2]=='e' and command[3]=='t':
-        try:
-            numba = int(filter(str.isdigit, command))
-            iterate(numba)
-            bot.sendChatAction(user_id, 'upload_photo')
-            bot.sendPhoto(user_id, open('Graph{}.png'.format(numba), 'rb'))
-            bot.sendChatAction(user_id, 'upload_document')
-            bot.sendDocument(user_id, open('Graph{}.svg'.format(numba)))
-            bot.sendMessage(user_id, 'Thank you for using collatzbot!')
-        except:
-            print('Will deal with that later')
-
-        finally:
-        # always delete images, even if an error is encountered, to prevent files from piling up
-            try:
-                os.remove('Graph{}.png'.format(numba))
-            except:
-                pass  # doesn't exist, good.
-            try:
-                os.remove('Graph{}.svg'.format(numba))
-            except:
-                pass  # doesn't exist, good.
         return
             
     try:
@@ -92,7 +61,7 @@ Also here are some interesting values to try out:\n\
         bot.sendMessage(user_id, 'Biggest value {} during step {}'.format(int(large_num), large_num_ind))
 
         bot.sendChatAction(user_id, 'upload_photo')
-        make_graph(num, steps, 'plot')
+        make_graph(num, steps, large_num_ind, large_num)
         bot.sendPhoto(user_id, open('Graph{}.png'.format(num), 'rb'))
         bot.sendChatAction(user_id, 'upload_document')
         bot.sendDocument(user_id, open('Graph{}.svg'.format(num)))
