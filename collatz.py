999f3da53edb9a8a95ae817194e7fb670db71d60
thecsw <sagindyk.urazayev@protonmail.ch>
2017-11-22 12:58:29 +0400

    Added comments

--- collatz.py
+++ collatz.py
@@ -1,13 +1,16 @@
+# OS library to delete rendered graphs
 import os
-
+# Matplotlib library to plot graphs (duh)
 import matplotlib.pyplot as plt
+# Telepot library to interact with Telegram's API
 import telepot
+# Telepot.loop library to get new messages
 from telepot.loop import MessageLoop
-
+# Here you include your Telegram API token. Refer to example.config.py and rename it config.py
 import config
-
+# Initializing telegram bot
 bot = telepot.Bot(config.token)
-
+# Function to calculate Collatz Conjecture
 def coll(n):
     steps = [n]
     while n != 1:
@@ -18,7 +21,7 @@ def coll(n):
         steps.append(n)
     steps.pop()
     return steps
-
+# Make graph
 def make_graph(num, steps, bigIn, big):
     plt.plot(range(len(steps)), steps, linestyle='-', linewidth=1)
     plt.xlabel('Step\'s number')
@@ -32,20 +35,20 @@ def make_graph(num, steps, bigIn, big):
     plt.savefig('Graph{}.png'.format(num), dpi=500, format='png')
     plt.savefig('Graph{}.svg'.format(num), dpi=500, format='svg')
     plt.clf()
-
+# Queries for new messages
 def handle(msg):
     user_id = msg['chat']['id']
-    command = msg['text'].encode('utf-8').lower()
-
+    command = msg['text'].encode('utf-8').lower() # Support lowercase
+    # Help
     if command == '/help':
         bot.sendMessage(user_id, 'Write any natural number and bot will send you the number of steps required to reach 1\n\
 Also here are some interesting values to try out:\n\
         27, 97, 871, 6171, 77031, 837799, 8400511, 670617279')
         return
-            
+    # Try to get a number from a message
     try:
         num = int(command)
-
+    # If not a number, report
     except ValueError:
         bot.sendMessage(user_id, 'Not a number!')
         return
@@ -66,10 +69,10 @@ Also here are some interesting values to try out:\n\
         bot.sendChatAction(user_id, 'upload_document')
         bot.sendDocument(user_id, open('Graph{}.svg'.format(num)))
         bot.sendMessage(user_id, 'Thank you for using collatzbot!')
-
+    # Something bad happened
     except Exception as e:
         bot.sendMessage(user_id, 'Error.')
-
+    # Delete files in any case
     finally:
         # always delete images, even if an error is encountered, to prevent files from piling up
         try:
@@ -81,5 +84,5 @@ Also here are some interesting values to try out:\n\
         except FileNotFoundError:
             pass  # doesn't exist, good.
 
-
+# Get new messages, always
 MessageLoop(bot, handle).run_forever()
